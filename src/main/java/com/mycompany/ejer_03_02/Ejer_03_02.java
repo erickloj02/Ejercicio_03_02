28b6f72046cfbf6d919d4c8e4d02d61526227903
PC <PC@LojanoCollantes.home>
2022-05-20 01:30:21 -0500

    Ejer_03_02_Terminado

--- src/main/java/com/mycompany/ejer_03_02/Ejer_03_02.java
+++ src/main/java/com/mycompany/ejer_03_02/Ejer_03_02.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.mycompany.ejer_03_02;
 
-/**
- *
- * @author PC
- */
 public class Ejer_03_02 {
 
     public static void main(String[] args) {
